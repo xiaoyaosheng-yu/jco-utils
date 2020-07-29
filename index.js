77c214e2e4570ab78aad02fc61cc0414b56ff2da
喻滔 <yu_tao@leapmotor.com>
2020-07-29 16:47:30 +0800

    新增日期库

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let stringUtils = require('./lib/string');
 let numberUtils = require('./lib/number');
 let checkUtils = require('./lib/check');
+let dateUtils = require('./lib/date');
 class JcoUtils {
   constructor () {
     this.String = stringUtils;
     this.Check = checkUtils;
     this.Number = numberUtils;
+    this.Date = dateUtils;
   }
 };
 
