1cd0966d9c60d0d9a582a6c603eaccafdb7a9f75
喻滔 <yu_tao@leapmotor.com>
2020-07-30 15:19:16 +0800

    新增url类

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ let stringUtils = require('./lib/string');
 let numberUtils = require('./lib/number');
 let checkUtils = require('./lib/check');
 let dateUtils = require('./lib/date');
+let urlUtils = require('./lib/url');
 class JcoUtils {
   constructor () {
     this.String = stringUtils;
     this.Check = checkUtils;
     this.Number = numberUtils;
     this.Date = dateUtils;
+    this.Url = urlUtils;
   }
 };
 
--- readme.md
+++ readme.md
@@ -1,4 +1,19 @@
 # JS公共方法库
 
 ### 安装
-npm i jco-utils
+```npm i jco-utils```
+
+### 使用
+```javascript
+const jcoUtils = require('jco-utils');
+jcoUtils.Check.isEmail('405263430@qq.com');
+```
+
+### 分类
+```javascript
+jcoUtils.String
+jcoUtils.Check
+jcoUtils.Number
+jcoUtils.Date
+jcoUtils.Url
+```
